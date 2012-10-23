cc3277eca5c2ac76f7aa10b4e8337ac1d4835fed
Catalin Manolescu <cc.manolescu@gmail.com>
2012-10-23 17:03:37 +0300

    fix remote swarm:

--- testSwarms/RemoteEcho.js
+++ testSwarms/RemoteEcho.js
@@ -7,8 +7,11 @@ var remoteEcho = {
     vars:{
         echoSource:null
     },
-    start:function () {
+    start:function (param1, param2) {
+        var assert              = require('assert');
         cprint("Started remote echo " + this.getSessionId());
+        assert.equal(param1,'testParam1');
+        assert.equal(param2,'testParam2');
         this.echoSource = thisAdapter.nodeName ;
         this.swarm("notify", this.getSessionId());
     }
