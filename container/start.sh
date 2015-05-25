5dc96170e306de1312747907e2f60c9fa2c89e08
salboaie <master1!23>
2015-05-25 17:17:37 +0300

    docker support, wip

--- container/start.sh
+++ container/start.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-export NODE_PATH=/usmed/server/
-export SWARM_NODE_TYPE='spiridon'
-export SWARM_PATH=/usmed/server/
-export PATH=$PATH:/usmed/dcm4che/dcm4che-2.0.28/bin/
-http-server /usmed/web/USMED/www/ -p 80&
+export NODE_PATH=/SwarmESB/
+export SWARM_NODE_TYPE='demo'
+export SWARM_PATH=/SwarmESB/
 http-server /usmed/server/admin/public/ -p 8000&
-node /usmed/server/adapters/usmedLaunch.js
+node /SwarmESB/adapters/demoLaunch.js
