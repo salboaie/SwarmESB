5bc4dc15d18f0cfdb41299b11389e6617db1dc80
Cosmin <cosmin@localhost.localdomain>
2017-11-07 13:44:31 +0200

    mysqlPersistence config updated

--- autolib/mysqlPersistence.js
+++ autolib/mysqlPersistence.js
@@ -1,24 +1,28 @@
-var mysql     = require('mysql');
+var mysql = require('mysql');
 var container = require('safebox').container;
 var apersistence = require('apersistence');
 
 var connectionSettings = {
-    connectionLimit:10,
-    host     : thisAdapter.config.Core.mysqlHost,
-    port     : thisAdapter.config.Core.mysqlPort,
-    user     : 'root',
-    password : thisAdapter.config.Core.mysqlDatabasePassword,
-    database : thisAdapter.config.Core.mysqlDatabaseName
+	"host": "localhost",
+	"port": "3306",
+	"user": "root",
+	"password": "",
+	"database": "swarm",
+	"connectionLimit": 2
+};
+
+if(thisAdapter.config.Core && thisAdapter.config.Core.mysqlConfig){
+	connectionSettings = thisAdapter.config.Core.mysqlConfig;
 }
 
 var mysqlConnection = mysql.createPool(connectionSettings);
-container.resolve('mysqlConnection',mysqlConnection);
+container.resolve('mysqlConnection', mysqlConnection);
 
-container.declareDependency("mysqlPersistence",['mysqlConnection'],function(outOfService,mysqlConnection){
-    if(outOfService){
-        console.log("MySQL persistence failed");
-    }else{
-        console.log("Initialising MySQL persistence");
-        return apersistence.createMySqlPersistence(mysqlConnection);
-    }
+container.declareDependency("mysqlPersistence", ['mysqlConnection'], function (outOfService, mysqlConnection) {
+	if (outOfService) {
+		console.log("MySQL persistence failed");
+	} else {
+		console.log("Initialising MySQL persistence");
+		return apersistence.createMySqlPersistence(mysqlConnection);
+	}
 });
